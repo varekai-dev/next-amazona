a71fb9e84eb729d3cd060e2b4a6695e581a7aa3a
varekai-dev <serhijsav@gmail.com>
2021-08-12 00:00:59 +0300

    add darkTheme

--- components/Layout.js
+++ components/Layout.js
@@ -1,67 +1,90 @@
-import { AppBar, Container, createMuiTheme, CssBaseline, Link, ThemeProvider, Toolbar, Typography } from '@material-ui/core';
-import Head from 'next/head';
-import NextLink from 'next/link';
-import React from 'react';
-import useStyles from '../utils/styles';
+import {
+  AppBar,
+  Container,
+  createTheme,
+  CssBaseline,
+  Link,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  Switch,
+} from "@material-ui/core";
+import Head from "next/head";
+import NextLink from "next/link";
+import React from "react";
+import useStyles from "../utils/styles";
+import { Store } from "../utils/Store";
+import Cookies from "js-cookie";
 
 const Layout = ({ title, children, description }) => {
-	const theme = createMuiTheme({
-		typography: {
-			h1: {
-				fontSize: '1.6rem',
-				fontWeight: 400,
-				margin: '1rem 0'
-			},
-			h1: {
-				fontSize: '1.4rem',
-				fontWeight: 400,
-				margin: '1rem 0'
-			}
-		},
-		palette: {
-			type: 'light',
-			primary: {
-				main: '#f0c000'
-			},
-			secondary: {
-				main: '#208080'
-			}
-		}
-	});
-	const s = useStyles();
-	return (
-		<div>
-			<Head>
-				<title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
-				{description && <meta name="description" content={description} />}
-			</Head>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				<AppBar position="static" className={s.navbar}>
-					<Toolbar>
-						<NextLink href="/" passHref>
-							<Link>
-								<Typography className={s.brand}>amazona</Typography>
-							</Link>
-						</NextLink>
-						<div className={s.grow}></div>
-						<div>
-							<NextLink href="/cart" passHref>
-								<Link>Cart</Link>
-							</NextLink>
-							<NextLink href="/login" passHref>
-								<Link>Login</Link>
-							</NextLink>
-						</div>
-					</Toolbar>
-				</AppBar>
-				<Container className={s.main}>{children}</Container>
-				<footer className={s.footer}>
-					<Typography>All rights reserved. Next Amazona</Typography>
-				</footer>
-			</ThemeProvider>
-		</div>
-	);
+  const { state, dispatch } = React.useContext(Store);
+  const { darkMode } = state;
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h1: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+    },
+    palette: {
+      type: darkMode ? "dark" : "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
+  const s = useStyles();
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+    const newDarkMode = !darkMode;
+    Cookies.set("darkMode", newDarkMode ? "ON" : "OFF");
+  };
+  return (
+    <div>
+      <Head>
+        <title>{title ? `${title} - Next Amazona` : "Next Amazona"}</title>
+        {description && <meta name="description" content={description} />}
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={s.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={s.brand}>amazona</Typography>
+              </Link>
+            </NextLink>
+            <div className={s.grow}></div>
+            <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={s.main}>{children}</Container>
+        <footer className={s.footer}>
+          <Typography>All rights reserved. Next Amazona</Typography>
+        </footer>
+      </ThemeProvider>
+    </div>
+  );
 };
 
 export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
-import { useEffect } from 'react';
-import '../styles/globals.css';
+import { useEffect } from "react";
+import { StoreProvider } from "../utils/Store";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-	useEffect(() => {
-		const jssStyles = document.querySelector('#jss-server-side');
-		if (jssStyles) {
-			jssStyles.parentElement.removeChild(jssStyles);
-		}
-	}, []);
-	return <Component {...pageProps} />;
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
 
 export default MyApp;
