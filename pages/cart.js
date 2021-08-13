8e125efb027e1503c73acde56e4bbb24f9c34f24
varekaiwork <serhiisaviukwork@gmail.com>
2021-08-13 17:00:29 +0300

    add

--- pages/cart.js
+++ pages/cart.js
@@ -1,15 +1,24 @@
 import { Grid, Table, TableContainer, Typography, TableHead, TableRow, TableCell, TableBody, Link, Select, MenuItem, Button, Card, List, ListItem } from '@material-ui/core';
 import React, { useContext } from 'react';
+import dynamic from 'next/dynamic';
 import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
 import NextLink from 'next/link';
 import Image from 'next/image';
+import axios from 'axios';
 
-export default function Cart() {
-	const { state } = useContext(Store);
+function CartScreen() {
+	const { state, dispatch } = useContext(Store);
 	const {
 		cart: { cartItems }
 	} = state;
+	const updateCartHandler = async (item, quantiy) => {
+		const { data } = await axios(`/api/products/${item._id}`);
+		if (data.countInStock <= 0) {
+			window.alert('Sorry. Product is out of stock');
+		}
+		dispatch({ type: 'CART_ADD_ITEM', payload: { ...item, quantiy } });
+	};
 	return (
 		<Layout title="Shopping Cart">
 			<Typography component="h1" variant="h1">
@@ -45,7 +54,7 @@ export default function Cart() {
 												</NextLink>
 											</TableCell>
 											<TableCell align="center">
-												<Select value={item.quantiy}>
+												<Select value={item.quantiy} onChange={(e) => updateCartHandler(item, e.target.value)}>
 													{[...Array(item.countInStock).keys()].map((x) => (
 														<MenuItem key={x + 1} value={x + 1}>
 															{x + 1}
@@ -70,7 +79,7 @@ export default function Cart() {
 							<List>
 								<ListItem variant="h2">
 									<Typography>
-										Subtotal ({cartItems.reduce((a, c) => a + c.quantiy, 0)}) items : ${cartItems.reduce((a, c) => a + c.price, 0)}
+										Subtotal ({cartItems.reduce((a, c) => a + c.quantiy, 0)}) items : ${cartItems.reduce((a, c) => a + c.quantiy * c.price, 0)}
 									</Typography>
 								</ListItem>
 								<ListItem>
@@ -86,3 +95,5 @@ export default function Cart() {
 		</Layout>
 	);
 }
+
+export default dynamic(() => Promise.resolve(CartScreen), { ssr: false });
