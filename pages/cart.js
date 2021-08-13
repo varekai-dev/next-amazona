51458856ed72000665b8d80774ed89704c94bb80
varekai-dev <serhijsav@gmail.com>
2021-08-13 22:48:50 +0300

    finish cart page

--- pages/cart.js
+++ pages/cart.js
@@ -1,99 +1,135 @@
-import { Grid, Table, TableContainer, Typography, TableHead, TableRow, TableCell, TableBody, Link, Select, MenuItem, Button, Card, List, ListItem } from '@material-ui/core';
-import React, { useContext } from 'react';
-import dynamic from 'next/dynamic';
-import Layout from '../components/Layout';
-import { Store } from '../utils/Store';
-import NextLink from 'next/link';
-import Image from 'next/image';
-import axios from 'axios';
+import {
+  Grid,
+  Table,
+  TableContainer,
+  Typography,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Link,
+  Select,
+  MenuItem,
+  Button,
+  Card,
+  List,
+  ListItem,
+} from "@material-ui/core";
+import React, { useContext } from "react";
+import dynamic from "next/dynamic";
+import Layout from "../components/Layout";
+import { Store } from "../utils/Store";
+import NextLink from "next/link";
+import Image from "next/image";
+import axios from "axios";
 
 function CartScreen() {
-	const { state, dispatch } = useContext(Store);
-	const {
-		cart: { cartItems }
-	} = state;
-	const updateCartHandler = async (item, quantiy) => {
-		const { data } = await axios(`/api/products/${item._id}`);
-		if (data.countInStock <= 0) {
-			window.alert('Sorry. Product is out of stock');
-		}
-		dispatch({ type: 'CART_ADD_ITEM', payload: { ...item, quantiy } });
-	};
-	return (
-		<Layout title="Shopping Cart">
-			<Typography component="h1" variant="h1">
-				Shopping Cart
-			</Typography>
-			{cartItems.length === 0 ? (
-				<div>
-					Cart is empty. <NextLink href="/">Go shopping</NextLink>
-				</div>
-			) : (
-				<Grid container spacing={1}>
-					<Grid item md={9} xs={12}>
-						<TableContainer>
-							<Table>
-								<TableHead>
-									<TableRow>
-										<TableCell>Image</TableCell>
-										<TableCell>Name</TableCell>
-										<TableCell align="center">Quantiy</TableCell>
-										<TableCell align="center">Price</TableCell>
-										<TableCell align="center">Action</TableCell>
-									</TableRow>
-								</TableHead>
-								<TableBody>
-									{cartItems.map((item) => (
-										<TableRow key={item._id}>
-											<TableCell>
-												<Image src={item.image} alt={item.name} width={50} height={50} />
-											</TableCell>
-											<TableCell>
-												<NextLink href={`/product/${item.slug}`} passHref>
-													<Link>{item.name}</Link>
-												</NextLink>
-											</TableCell>
-											<TableCell align="center">
-												<Select value={item.quantiy} onChange={(e) => updateCartHandler(item, e.target.value)}>
-													{[...Array(item.countInStock).keys()].map((x) => (
-														<MenuItem key={x + 1} value={x + 1}>
-															{x + 1}
-														</MenuItem>
-													))}
-												</Select>
-											</TableCell>
-											<TableCell>${item.price}</TableCell>
-											<TableCell align="center">
-												<Button variant="contained" color="secondary">
-													X
-												</Button>
-											</TableCell>
-										</TableRow>
-									))}
-								</TableBody>
-							</Table>
-						</TableContainer>
-					</Grid>
-					<Grid item md={3} xs={12}>
-						<Card>
-							<List>
-								<ListItem variant="h2">
-									<Typography>
-										Subtotal ({cartItems.reduce((a, c) => a + c.quantiy, 0)}) items : ${cartItems.reduce((a, c) => a + c.quantiy * c.price, 0)}
-									</Typography>
-								</ListItem>
-								<ListItem>
-									<Button variant="contained" color="primary">
-										Check Out
-									</Button>
-								</ListItem>
-							</List>
-						</Card>
-					</Grid>
-				</Grid>
-			)}
-		</Layout>
-	);
+  const { state, dispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+  const updateCartHandler = async (item, quantity) => {
+    const { data } = await axios(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert("Sorry. Product is out of stock");
+      return;
+    }
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...item, quantity } });
+  };
+  const removeItemHandler = (item) => {
+    dispatch({ type: "CART_REMOVE_ITEM", payload: item });
+  };
+  return (
+    <Layout title="Shopping Cart">
+      <Typography component="h1" variant="h1">
+        Shopping Cart
+      </Typography>
+      {cartItems.length === 0 ? (
+        <div>
+          Cart is empty. <NextLink href="/">Go shopping</NextLink>
+        </div>
+      ) : (
+        <Grid container spacing={1}>
+          <Grid item md={9} xs={12}>
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Image</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell align="center">Quantiy</TableCell>
+                    <TableCell align="center">Price</TableCell>
+                    <TableCell align="center">Action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {cartItems.map((item) => (
+                    <TableRow key={item._id}>
+                      <TableCell>
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        />
+                      </TableCell>
+                      <TableCell>
+                        <NextLink href={`/product/${item.slug}`} passHref>
+                          <Link>{item.name}</Link>
+                        </NextLink>
+                      </TableCell>
+                      <TableCell align="center">
+                        <Select
+                          value={item.quantity}
+                          onChange={(e) =>
+                            updateCartHandler(item, e.target.value)
+                          }
+                        >
+                          {[...Array(item.countInStock).keys()].map((x) => (
+                            <MenuItem key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </TableCell>
+                      <TableCell>${item.price}</TableCell>
+                      <TableCell align="center">
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          onClick={() => removeItemHandler(item)}
+                        >
+                          X
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+          <Grid item md={3} xs={12}>
+            <Card>
+              <List>
+                <ListItem variant="h2">
+                  <Typography>
+                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)})
+                    items : $
+                    {cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
+                  </Typography>
+                </ListItem>
+                <ListItem>
+                  <Button variant="contained" color="primary">
+                    Check Out
+                  </Button>
+                </ListItem>
+              </List>
+            </Card>
+          </Grid>
+        </Grid>
+      )}
+    </Layout>
+  );
 }
 
 export default dynamic(() => Promise.resolve(CartScreen), { ssr: false });
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,113 +1,137 @@
-import React from 'react';
-import Layout from '../../components/Layout';
-import NextLink from 'next/link';
-import Image from 'next/image';
-import { Grid, Link, List, ListItem, Typography, Card, Button } from '@material-ui/core';
-import useStyles from '../../utils/styles';
-import db from '../../utils/db';
-import Product from '../../models/Product';
-import axios from 'axios';
-import { Store } from '../../utils/Store';
-import { useRouter } from 'next/dist/client/router';
+import React from "react";
+import Layout from "../../components/Layout";
+import NextLink from "next/link";
+import Image from "next/image";
+import {
+  Grid,
+  Link,
+  List,
+  ListItem,
+  Typography,
+  Card,
+  Button,
+} from "@material-ui/core";
+import useStyles from "../../utils/styles";
+import db from "../../utils/db";
+import Product from "../../models/Product";
+import axios from "axios";
+import { Store } from "../../utils/Store";
+import { useRouter } from "next/dist/client/router";
 
 const ProductScreen = ({ product }) => {
-	const router = useRouter();
-	const { dispatch } = React.useContext(Store);
-	const s = useStyles();
-	if (!product) {
-		return <div>Product Not Found</div>;
-	}
-	const addToCartHandler = async () => {
-		const { data } = await axios(`/api/products/${product._id}`);
-		if (data.countInStock <= 0) {
-			window.alert('Sorry. Product is out of stock');
-		}
-		dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantiy: 1 } });
-		router.push('/cart');
-	};
-	return (
-		<Layout title={product.name} description={product.description}>
-			<div className={s.section}>
-				<NextLink href="/" passHref>
-					<Typography>
-						<Link> Back to products</Link>
-					</Typography>
-				</NextLink>
-			</div>
-			<Grid container spacing={1}>
-				<Grid item md={6} xs={12}>
-					<Image src={product.image} alt={product.name} width={640} height={640} />
-				</Grid>
-				<Grid item md={3} xs={12}>
-					<List>
-						<ListItem>
-							<Typography component="h1" variant="h1">
-								{product.name}
-							</Typography>
-						</ListItem>
-						<ListItem>
-							<Typography> Category: {product.category}</Typography>
-						</ListItem>
-						<ListItem>
-							<Typography>Brand: {product.brand} </Typography>
-						</ListItem>
-						<ListItem>
-							<Typography>
-								Rating: {product.rating} stars ({product.numReviews} reviews)
-							</Typography>
-						</ListItem>
-						<ListItem>
-							<Typography> Description: {product.description}</Typography>
-						</ListItem>
-					</List>
-				</Grid>
-				<Grid item md={3} xs={12}>
-					<Card>
-						<List>
-							<ListItem>
-								<Grid container>
-									<Grid item xs={6}>
-										<Typography>Price</Typography>
-									</Grid>
-									<Grid item xs={6}>
-										<Typography>${product.price}</Typography>
-									</Grid>
-								</Grid>
-							</ListItem>
-							<ListItem>
-								<Grid container>
-									<Grid item xs={6}>
-										<Typography>Status</Typography>
-									</Grid>
-									<Grid item xs={6}>
-										<Typography>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</Typography>
-									</Grid>
-								</Grid>
-							</ListItem>
-							<ListItem>
-								<Button fullWidth variant="contained" color="primary" onClick={addToCartHandler}>
-									Add to cart
-								</Button>
-							</ListItem>
-						</List>
-					</Card>
-				</Grid>
-			</Grid>
-		</Layout>
-	);
+  const router = useRouter();
+  const { state, dispatch } = React.useContext(Store);
+  const s = useStyles();
+  if (!product) {
+    return <div>Product Not Found</div>;
+  }
+  const addToCartHandler = async () => {
+    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      window.alert("Sorry. Product is out of stock");
+      return;
+    }
+
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
+    router.push("/cart");
+  };
+  return (
+    <Layout title={product.name} description={product.description}>
+      <div className={s.section}>
+        <NextLink href="/" passHref>
+          <Typography>
+            <Link> Back to products</Link>
+          </Typography>
+        </NextLink>
+      </div>
+      <Grid container spacing={1}>
+        <Grid item md={6} xs={12}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+          />
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <List>
+            <ListItem>
+              <Typography component="h1" variant="h1">
+                {product.name}
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography> Category: {product.category}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Brand: {product.brand} </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>
+                Rating: {product.rating} stars ({product.numReviews} reviews)
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography> Description: {product.description}</Typography>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <Card>
+            <List>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Price</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography>${product.price}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Status</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography>
+                      {product.countInStock > 0 ? "In stock" : "Unavailable"}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  onClick={() => addToCartHandler(product)}
+                >
+                  Add to cart
+                </Button>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
 };
 
 export default ProductScreen;
 
 export async function getServerSideProps(context) {
-	const { params } = context;
-	const { slug } = params;
-	await db.connect();
-	const product = await Product.findOne({ slug }).lean();
-	await db.disconnect();
-	return {
-		props: {
-			product: db.convertDocToObject(product)
-		}
-	};
+  const { params } = context;
+  const { slug } = params;
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+  return {
+    props: {
+      product: db.convertDocToObject(product),
+    },
+  };
 }
