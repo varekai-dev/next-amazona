82bf4161da5985c7d3076d806e5679309c8f15b0
varekai-dev <serhijsav@gmail.com>
2021-08-16 23:17:45 +0300

    login api

--- pages/login.js
+++ pages/login.js
@@ -1,37 +1,89 @@
-import { List, ListItem, Typography, TextField, Button, Link } from '@material-ui/core';
-import React from 'react';
-import Layout from '../components/Layout';
-import useStyles from '../utils/styles';
-import NextLink from 'next/link';
+import {
+  List,
+  ListItem,
+  Typography,
+  TextField,
+  Button,
+  Link,
+} from "@material-ui/core";
+import React from "react";
+import Layout from "../components/Layout";
+import useStyles from "../utils/styles";
+import NextLink from "next/link";
+import axios from "axios";
+import { Store } from "../utils/Store";
+import { useRouter } from "next/dist/client/router";
+import Cookies from "js-cookie";
 
 export default function Login() {
-	const classes = useStyles();
-	return (
-		<Layout title="Login">
-			<form className={classes.form}>
-				<Typography component="h1" variant="h1">
-					Login
-				</Typography>
-				<List>
-					<ListItem>
-						<TextField variant="outlined" fullWidth id="email" label="Email" inputProps={{ type: 'email' }}></TextField>
-					</ListItem>
-					<ListItem>
-						<TextField variant="outlined" fullWidth id="email" label="Password" inputProps={{ type: 'password' }}></TextField>
-					</ListItem>
-					<ListItem>
-						<Button variant="contained" type="submit" fullWidth color="primary">
-							Login
-						</Button>
-					</ListItem>
-					<ListItem>
-						Don't have an account ?{' '}
-						<NextLink href="/register" passHref>
-							<Link> Register</Link>
-						</NextLink>
-					</ListItem>
-				</List>
-			</form>
-		</Layout>
-	);
+  const router = useRouter();
+  const { redirect } = router.query;
+  const { state, dispatch } = React.useContext(Store);
+  const { userInfo } = state;
+  if (userInfo) {
+    router.push("/");
+  }
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const classes = useStyles();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/users/login", {
+        email,
+        password,
+      });
+      console.log(data);
+      dispatch({ type: "USER_LOGIN", payload: data });
+      Cookies.set("userInfo", data);
+      router.push(redirect || "/");
+    } catch (err) {
+      alert(err.response.data ? err.response.data.message : err.message);
+    }
+  };
+  return (
+    <Layout title="Login">
+      <form onSubmit={submitHandler} className={classes.form}>
+        <Typography component="h1" variant="h1">
+          Login
+        </Typography>
+        <List>
+          <ListItem>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="email"
+              label="Email"
+              inputProps={{ type: "email" }}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></TextField>
+          </ListItem>
+          <ListItem>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="email"
+              label="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              inputProps={{ type: "password" }}
+            ></TextField>
+          </ListItem>
+          <ListItem>
+            <Button variant="contained" type="submit" fullWidth color="primary">
+              Login
+            </Button>
+          </ListItem>
+          <ListItem>
+            Don't have an account ?{" "}
+            <NextLink href="/register" passHref>
+              <Link> Register</Link>
+            </NextLink>
+          </ListItem>
+        </List>
+      </form>
+    </Layout>
+  );
 }
--- pages/shipping.js
+++ pages/shipping.js
@@ -1,8 +1,17 @@
-import { useRouter } from 'next/dist/client/router';
-import React from 'react';
+import { useRouter } from "next/dist/client/router";
+import React from "react";
+import { Store } from "../utils/Store";
 
 export default function Shipping() {
-	const router = useRouter();
-	router.push('/login');
-	return <div></div>;
+  const router = useRouter();
+  const { state } = React.useContext(Store);
+  const { userInfo } = state;
+  React.useEffect(() => {
+    if (!userInfo) {
+      router.push("/login?redirect=/shipping");
+    }
+  }, []);
+
+  router.push("/login");
+  return <div></div>;
 }
